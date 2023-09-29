ce52d94927bab714d3f50a774bfe5189402e4e71
IRONMELTS <87653130+IRONMELTS@users.noreply.github.com>
2023-09-29 23:16:51 +0530

    idk man my brain

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 f=open("mark.txt","r")
 lines=f.readlines()
 c=0
+cur_line=0
+
 for i in lines:
+    cur_line+=1
     if i[0].isdigit():
         c+=1
         each_line=i.split()
-        print(each_line)
+        details=each_line
+        mark_grade=lines[cur_line]
+        print(details)
+        print(mark_grade.strip())
+
 print("Totally got: ",c)
 f.close()
